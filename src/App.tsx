095013575a11299858813bf3223c47305dc24fe9
Mjkwebdev <khanmona17321@gmail.com>
2025-03-01 10:39:56 +0500

    Initial Commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import Hero from './MainComponent/Hero'
+
+function App() {
+
+  return (
+    <Hero/>
+  )
+}
+
+export default App
---
+++ src/BeachImgComponent.tsx
@@ -0,0 +1,8 @@
+
+const BeachImgComponent = () => {
+  return (
+    <img src="./src/assets/beachBg.jpg" alt="my-image" />
+  )
+}
+
+export default BeachImgComponent
