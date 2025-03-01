4e96272229ea045ab7aff5aefdddea324d2f19bc
Mjkwebdev <khanmona17321>
2025-03-01 11:52:01 +0500

    Reversing it

---
+++ src/MainComponent/Hero.tsx
@@ -0,0 +1,19 @@
+
+import BeachImgComponent from "../BeachImgComponent"
+const Hero = () => {
+  return (
+    <>
+    <div className='flex w-screen h-screen bg-[green]'>
+      <div className='w-1/2 bg-[#AD8330] flex flex-col justify-center'>
+        <h1 className='text-4xl font-bold p-10'> Experience the Perfect Beach Day with Ease</h1>
+      <p className='text-lg'> Reserve sunbeds, enjoy add-ons, and relax without hassle</p>
+      </div>
+      <div className='w-1/2 bg-cover' style={{backgroundImage: `url(${BeachImgComponent})`}}>
+    <img src="${beachImgComponent}" alt="Beach image" />
+      </div>
+    </div>
+    </>
+  )
+}
+
+export default Hero
