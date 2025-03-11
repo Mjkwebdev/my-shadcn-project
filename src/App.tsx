2ad737f07e93f5623aa175040384a20d91e455a3
zia243 <mziamalick@gmail.com>
2025-03-11 15:24:34 +0500

    Booking page added with login form and Booking card

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Hero from "./MainComponent/Hero";
 import SearchPage from "./MainComponent/Search";
 import DetailPage from "./MainComponent/Detail";
+import Booking from "./MainComponent/Booking";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
         <Route path="/" element={<Hero/>} />
         <Route path="/search" element={<SearchPage />} />
         <Route path="/detail/:slug" element={<DetailPage/>} />
+        <Route path="/booking" element={<Booking/>} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/MainComponent/Booking.tsx
@@ -0,0 +1,51 @@
+import { CalendarIcon } from "lucide-react";
+import {  UserIcon } from "@heroicons/react/24/outline";
+import LoginForm from "./LoginForm";
+
+const Booking = () => {
+  return (
+    <div className="flex items-start gap-6 my-8 mx-24">
+    <div className="w-[35%] bg-white shadow-lg rounded-lg border border-gray-200  p-6">
+      <div className="flex items-center border-b pb-3">
+        <img
+           src="/src/assets/avatar.jpg"
+          alt="Beach Club"
+          className="w-16 h-16 rounded-full mr-3"
+        />
+        <div>
+          <h2 className="text-lg font-[Poppins,Sans-serif] font-semibold">Calabro Beach Club</h2>
+          <p className="text-sm font-[Poppins,Sans-serif] text-gray-500">9 promenade de la mer, Menton, France</p>
+        </div>
+      </div>
+      <div className="flex justify-between items-center py-3 border-b">
+        <div className="flex gap-2 items-center text-gray-700 font-semibold font-[Poppins,Sans-serif]">
+          
+          <UserIcon className="h-6 w-6 text-blue-700 " />
+          <span>2</span>
+        </div>
+        <div className="flex gap-3 items-center text-gray-700 font-semibold font-[Poppins,Sans-serif]">
+          {/* <FaRegCalendarAlt className="mr-1" /> */}
+          <CalendarIcon size={20}  className="text-blue-700"/>
+          <span>01 May 2025</span>
+        </div>
+      </div>
+      <div className="py-3 flex flex-col gap-6">
+        <div className="flex justify-between text-gray-700">
+          <span>Sunbed - All rows x2</span>
+          <span className="font-semibold">40,00 €</span>
+        </div>
+        <div className="flex justify-between text-gray-500 mt-1">
+          <span>Parasol (1 per 2 pers) x1</span>
+          <span className="font-semibold">Include</span>
+        </div>
+      </div>
+    </div>
+    <div className="w-[65%]">
+        
+     <LoginForm/>
+    </div>
+    </div>
+  );
+};
+
+export default Booking;
